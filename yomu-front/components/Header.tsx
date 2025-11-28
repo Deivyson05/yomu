5b8d020609464536818afcf15a7ad7237aa851a3
Deivyson Ricardo Silva dos Santos <131066052+Deivyson05@users.noreply.github.com>
2025-11-27 21:02:04 -0300

    corrigindo bugs

--- yomu-front/components/Header.tsx
+++ yomu-front/components/Header.tsx
@@ -1,7 +1,12 @@
 'use client'
 import { getSessionData } from '@/core/sStorage'
+import { useEffect, useState } from 'react';
 
 export function Header() {
+  const [nomeUsuario, setNomeUsuario] = useState("");
+  useEffect(()=>{
+    setNomeUsuario(getSessionData('user').nomeUsuario);
+  })
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-white/80 backdrop-blur-md border-b border-gray-200/50 py-4 sm:py-4">
       <div className="w-full flex justify-between items-center px-4 sm:px-6 max-w-6xl mx-auto">
