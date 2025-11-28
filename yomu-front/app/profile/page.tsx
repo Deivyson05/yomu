13931fa75e4a4de8ea523606ea1d7efbf44cb6e8
Deivyson Ricardo Silva dos Santos <131066052+Deivyson05@users.noreply.github.com>
2025-11-27 21:52:03 -0300

    buggggs

--- yomu-front/app/profile/page.tsx
+++ yomu-front/app/profile/page.tsx
@@ -31,13 +31,13 @@ export default function Profile() {
       try {
         const response = await getPerfil();
         setPerfil({
-          nome: response.username,
+          nome: response.nomeUsuario,
           xpAtual: response.xpTotal,
           xpTotal: 800,
           level: response.nivelAtual,
           rank: 239,
           seguidores: 57,
-          fotoPerfil: `https://yomu-api-0tys.onrender.com/yomu${response.foto}`,
+          fotoPerfil: `https://yomu-api-0tys.onrender.com/yomu${response.fotoPerfil}`,
           badges: [
             { id: 1, nome: "Badge 1", img: "/images/badge1.png" },
             { id: 2, nome: "Badge 2", img: "/images/badge2.png" },
