2612be31fd6a53c71c272e64ac08bdaae2c2fed5
Deivyson Ricardo Silva dos Santos <131066052+Deivyson05@users.noreply.github.com>
2025-11-27 21:26:33 -0300

    bug

--- yomu-front/components/Header.tsx
+++ yomu-front/components/Header.tsx
@@ -11,7 +11,7 @@ export function Header() {
     <header className="fixed top-0 left-0 right-0 z-50 bg-white/80 backdrop-blur-md border-b border-gray-200/50 py-4 sm:py-4">
       <div className="w-full flex justify-between items-center px-4 sm:px-6 max-w-6xl mx-auto">
         <h1 className="text-xl sm:text-2xl lg:text-3xl font-bold text-gray-900">
-          Olá, {getSessionData('user').nomeUsuario}!
+          Olá, {nomeUsuario}!
         </h1>
 
         {/* dexair os icones à direita */}
